71a6282743bf51fc81bc4cc73473daff22db600f
erickhansx <erickhansx@gmail.com>
2023-09-05 05:34:48 -0600

    Create basic crud actions update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
